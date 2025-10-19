f5f80304b0fb9f613f2322d71d09b73b2a18d8e0
RavikumarRDN <ravikumarrd08@gmail.com>
2025-10-19 12:27:32 +0530

    Implemented the extent report concepts in the test cases and also updated in the jenkins to produce the extent report.

--- src/test/java/StockExchange/BaseStock.java
+++ src/test/java/StockExchange/BaseStock.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 /**
  * Ravikumar 14/10/2025 Implementing the base class and home page
  */
@@ -14,27 +17,52 @@ public class BaseStock {
 	
 	WebDriver driver;
 	HomePage hp;
+	ExtentReports extent;
+	ExtentSparkReporter reporter;
+	
+	@BeforeTest
+	public void initialization() {
+		driver=new ChromeDriver();
+		hp=new HomePage(driver);
+		String path=System.getProperty("user.dir")+"\\Reports\\AutomationResult.html";
+		reporter=new ExtentSparkReporter(path);
+		reporter.config().setReportName("National Stock Exchange Results");
+		reporter.config().setDocumentTitle("WebAutomation");
+		
+		extent=new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Ravikumar");
+		driver.get("https://www.nseindia.com/");
+		driver.manage().window().maximize();
+	}
+	
+	@AfterTest
+	public void close() {
+		driver.close();
+		driver.quit();
+	}
 	
 	@Test
 	public void openingAboutNSE() {
-		driver=new ChromeDriver();
-		hp=new HomePage(driver);
-		driver.get("https://www.nseindia.com/");
-		driver.manage().window().maximize();
+		extent.createTest("openingAboutNSE");
 		hp.closeNotifyWindow();
 		hp.openAboutNSE();
+		extent.flush();
 		
 	}
 	
 	@Test
 	public void openingList() {
+		extent.createTest("openingList");
 		hp.openingList();
+		extent.flush();
 	}
 	
 	@Test
 	public void openingInvest() {
+		extent.createTest("openingInvest");
 		hp.openingInvest();
-		//Based on the web hook checking going to changes the code
+		extent.flush();
 	}
 
 }
